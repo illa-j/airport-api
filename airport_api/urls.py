7e65d42d6d8b110401d8f876512188f962dbbe52
illa-j <iliacdmn@gmail.com>
2026-01-19 23:13:37 +0200

    chore: create user app

--- airport_api/urls.py
+++ airport_api/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/airport/", include("airport_service.urls")),
+    path("api/user/", include("user.urls")),
 ]
