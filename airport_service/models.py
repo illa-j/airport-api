090ddb1f4eb327cc728003115b19d78e1ff12766
illa-j <iliacdmn@gmail.com>
2026-01-22 21:32:29 +0200

    feat: add models: Route, Airport, City, Country and make migrations for them

--- airport_service/models.py
+++ airport_service/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = ("name", "country")
+
+    def __str__(self):
+        return f"{self.name} - {self.country}"
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.ForeignKey(City, on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = ("name", "city")
+
+    def __str__(self):
+        return f"{self.name} ({self.city})"
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport,
+        on_delete=models.PROTECT,
+        related_name="routes_from"
+    )
+    destination = models.ForeignKey(
+        Airport,
+        on_delete=models.PROTECT,
+        related_name="routes_to"
+    )
+    distance = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ("source", "destination")
+        indexes = [
+            models.Index(fields=["source", "destination"]),
+        ]
+    def __str__(self):
+        return f"{self.source.name} -> {self.destination.name}: {self.distance}km"
